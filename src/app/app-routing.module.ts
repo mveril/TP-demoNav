b4a4443d3979ae232ede2c13c54e4d15d2209dc1
Mickaël Véril <mika.veril@wanadoo.fr>
2022-08-22 16:59:29 +0200

    Add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
+  {path:"", component:WelcomeComponent},
   {path:"welcome", component:WelcomeComponent},
-  {path:"profile", component:ProfileComponent}
+  {path:"profile", component:ProfileComponent},
+  {path:"**", component:NotFoundComponent}
 ];
 
 @NgModule({
